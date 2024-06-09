495901d54565f8995c21ff91acdf308068163f01
paulooosf <phpessoal2021@gmail.com>
2024-06-09 17:46:28 -0300

    feat: fazendo cards

--- src/Pages/Lista/index.jsx
+++ src/Pages/Lista/index.jsx
@@ -7,11 +7,49 @@ import DarkMode from "../../Components/DarkMode/index.jsx";
 function Lista() {
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
                 <div className="container">
-                        <DarkMode/>
+                    <DarkMode/>
+                    <div className="cards">
+                        <div className="card">
+                            <header className="card_titulo">
+                                <h3>Pizza de 4 queijos</h3>
+                            </header>
+                            <footer className="card_rodape">
+                                <div className="card_rodape_editar">
+                                    <button className="editar"><img src="src\assets\editar.png" alt=""/></button>
+                                    <button className="deletar"><img src="src\assets\deletar.png" alt=""/></button>
+                                </div>
+                                <button className="ver_receita">Ver receita</button>
+                            </footer>
+                        </div>
+                        <div className="card">
+                            <header className="card_titulo">
+                                <h3>Pizza de 4 queijos</h3>
+                            </header>
+                            <footer className="card_rodape">
+                                <div className="card_rodape_editar">
+                                    <button className="editar"><img src="src\assets\editar.png" alt=""/></button>
+                                    <button className="deletar"><img src="src\assets\deletar.png" alt=""/></button>
+                                </div>
+                                <button className="ver_receita">Ver receita</button>
+                            </footer>
+                        </div>
+                        <div className="card">
+                            <header className="card_titulo">
+                                <h3>Pizza de 4 queijos</h3>
+                            </header>
+                            <footer className="card_rodape">
+                                <div className="card_rodape_editar">
+                                    <button className="editar"><img src="src\assets\editar.png" alt=""/></button>
+                                    <button className="deletar"><img src="src\assets\deletar.png" alt=""/></button>
+                                </div>
+                                <button className="ver_receita">Ver receita</button>
+                            </footer>
+                        </div>
                     </div>
+                </div>
             </main>
         </div>
     );
