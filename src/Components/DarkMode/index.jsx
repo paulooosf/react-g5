fb4c34f698c4ed053c0dfc151e4a3b19c20aa7a8
Nicks-Souza <nicolas.souza990@gmail.com>
2024-06-08 22:17:34 -0300

    Fix: Mudando o lugar do botão do DarkMode

---
+++ src/Components/DarkMode/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Header from "../../Header/index.jsx";
+import "./style.css";
+import vector from "../../assets/Vector.png";
+import { useState, useEffect } from "react";
+
+const colors = ["transparent", "#2B2D42"];
+
+function DarkMode() {
+    const [position, setPosition] = useState(0)
+    const [cor, setCor] = useState(colors[position])
+
+    const click = () => {
+        setPosition(position === colors.length - 1 ? 0 : position + 1);
+    }
+
+    useEffect(() => {
+        setCor(colors[position]);
+    }, [position]);
+
+    return (
+        <div style={{ backgroundColor: cor }}>
+            <Header />
+            <main>
+                <div className="container">
+                    <div className="buttons">
+                        <button className='botao' onClick={click}>
+                            <img src={vector} alt="Modo escuro" />
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default DarkMode;
+
--- src/Pages/Lista/index.jsx
+++ src/Pages/Lista/index.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import Header from "../../Header/index.jsx";
-import vector from "../../assets/Vector.png";
-import { useState, useEffect } from "react";
+import "./style.css";
+import DarkMode from "../../Components/DarkMode/index.jsx";
 
-const colors = ["transparent", "#2B2D42"];
 
 function Lista() {
-    const [position, setPosition] = useState(0)
-    const [cor, setCor] = useState(colors[position])
-
-    const click = () => {
-        setPosition(position === colors.length - 1 ? 0 : position + 1);
-    }
-
-    useEffect(() => {
-        setCor(colors[position]);
-    }, [position]);
-
     return (
-        <div style={{ backgroundColor: cor }}>
+        <div>
             <Header />
             <main>
                 <div className="container">
-                    <div className="buttons">
-                        <button className='botao' onClick={click}>
-                            <img src={vector} alt="Modo escuro" />
-                        </button>
+                        <DarkMode/>
                     </div>
-                </div>
             </main>
         </div>
     );
 }
 
-export default Lista
+export default Lista;
